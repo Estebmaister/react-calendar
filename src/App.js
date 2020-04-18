e665df0d86586c96722f35f60f3df85913f851da
esteb <estebmaister@gmail.com>
2020-04-17 22:12:36 -0500

    :heavy_plus_sign: Adding moment library and calendar component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Calendar from './components/calendar/'
 
 function App() {
   return (
     <div className="App">
+      <Calendar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
---
+++ src/components/calendar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import moment from 'moment';
+
+export default class Calendar extends React.Component {
+    render() {
+        return (
+            <div className='calendar-container'>
+                <h2>Calendar</h2>
+            </div>
+        );
+    }
+}
