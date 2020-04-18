347c552d0142f952e4649a7bb9c6269f2cc03788
esteb <estebmaister@gmail.com>
2020-04-18 09:36:06 -0500

    :sparkles: Week days for the header

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import moment from 'moment';
+import './calendar.css';
+
+const weekdayshort = moment.weekdaysShort();
+
+let weekdayshortname = weekdayshort.map(day => {
+    return (
+      <th key={day} className="week-day">
+       {day}
+      </th>
+    );
+ });
 
 export default class Calendar extends React.Component {
     render() {
         return (
             <div className='calendar-container'>
                 <h2>Calendar</h2>
+                <div className='row'>
+                    {weekdayshortname}
+                </div>
             </div>
         );
     }
